3645105d49bd60a4c8f89504a64420723d31041f
Stanislav Ryabtsev <stanislav93ryabtsev@gmail.com>
2023-08-16 14:47:18 +0300

    added def binary search

--- exersise.py
+++ exersise.py
@@ -2,3 +2,22 @@ a = 5
 b = 7
 c = 8
 print((a*b*c))
+
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = (low + high) / 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+binary_search(my_list, -1)
